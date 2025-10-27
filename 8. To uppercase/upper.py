ae1b6a4c9a019bbb9819b0163382013afb81121c
David <ogbonnad379@gmail.com>
2025-10-27 10:28:10 +0100

    a function that prints a string in uppercase followed by a new line

---
+++ 8. To uppercase/upper.py
@@ -0,0 +1,10 @@
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
+
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
